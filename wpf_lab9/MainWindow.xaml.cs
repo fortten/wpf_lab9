ede7c22500dd3f1d94b8fd220e6977f75d5424b2
sergey leonov <redfortten@hotmail.com>
2021-11-25 22:40:02 +0300

    Добавьте файлы проекта.

---
+++ wpf_lab9/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_lab9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("Light.xaml", UriKind.Relative);
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Dark.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        #region Выбор шрифта
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+        #endregion
+
+        #region Выбор размера шрифта
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            int fontSize = Convert.ToInt32((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+        #endregion
+
+        #region Выбор стиля шрифта
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontWeight != FontWeights.Bold)
+                {
+                    textBox.FontWeight = FontWeights.Bold;
+                    (sender as Button).BorderBrush = Brushes.Black;
+                }
+                else
+                {
+                    textBox.FontWeight = FontWeights.Normal;
+                    (sender as Button).BorderBrush = Brushes.Transparent;
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontStyle != FontStyles.Italic)
+                {
+                    textBox.FontStyle = FontStyles.Italic;
+                    (sender as Button).BorderBrush = Brushes.Black;
+                }
+                else
+                {
+                    textBox.FontStyle = FontStyles.Normal;
+                    (sender as Button).BorderBrush = Brushes.Transparent;
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.TextDecorations != TextDecorations.Underline)
+                {
+                    textBox.TextDecorations = TextDecorations.Underline;
+                    (sender as Button).BorderBrush = Brushes.Black;
+                }
+                else
+                {
+                    textBox.TextDecorations = null;
+                    (sender as Button).BorderBrush = Brushes.Transparent;
+                }
+            }
+        }
+        #endregion
+
+        #region Выбор цвета шрифта
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+        #endregion
+
+        #region Меню "Файл"
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+    }
+}
